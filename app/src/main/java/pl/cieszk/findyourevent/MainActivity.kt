eb707a82278bef16c1ce98d3bbaabe14ca45177e
kamil.cieszkowski <cieszkens@gmail.com>
2024-01-25 16:45:31 +0100

    Change the login to the FirebaseUI, enable loging with Google, Mail and Facebook accounts

--- app/src/main/java/pl/cieszk/findyourevent/MainActivity.kt
+++ app/src/main/java/pl/cieszk/findyourevent/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
-import pl.cieszk.findyourevent.ui.auth.LoginScreen
-import pl.cieszk.findyourevent.ui.auth.RegistrationScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +16,10 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-            NavHost(navController = navController, startDestination = "login") {
-                composable("login") { LoginScreen(navController, auth) }
-                composable("registration") { RegistrationScreen(navController) }
-            }
+//            NavHost(navController = navController, startDestination = "login") {
+//                composable("login") { LoginScreen(navController, auth) }
+//                composable("registration") { RegistrationScreen(navController) }
+ //           }
         }
     }
 }
