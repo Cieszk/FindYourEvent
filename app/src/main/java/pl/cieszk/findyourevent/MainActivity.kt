5706efac8bf653477d17ef9d1340d83abb89cbb3
kamil.cieszkowski <cieszkens@gmail.com>
2024-02-12 12:37:39 +0100

    Add logo and app name for my SplashScreen

--- app/src/main/java/pl/cieszk/findyourevent/MainActivity.kt
+++ app/src/main/java/pl/cieszk/findyourevent/MainActivity.kt
@@ -3,11 +3,6 @@ package pl.cieszk.findyourevent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 
--- app/src/main/java/pl/cieszk/findyourevent/screens/splash/SplashScreen.kt
+++ app/src/main/java/pl/cieszk/findyourevent/screens/splash/SplashScreen.kt
@@ -1,22 +1,32 @@
 package pl.cieszk.findyourevent.screens.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.delay
+import pl.cieszk.findyourevent.R
 
 private const val SPLASH_TIMEOUT = 1000L
+
 @Composable
 fun SplashScreen(
     openAndPopUp: (String, String) -> Unit,
@@ -33,6 +43,13 @@ fun SplashScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            "Find Your Event",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Image(painter = painterResource(id = R.drawable.app_logo), contentDescription = "App logo")
         CircularProgressIndicator(color = MaterialTheme.colorScheme.onBackground)
     }
 
